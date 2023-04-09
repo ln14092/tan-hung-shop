e8ef752080e24f8bbd6cd58aea62c3a298576da0
langvan2003 <langthaovan2003@gmail.com>
2023-04-09 14:40:16 +0700

    Them mot so trang khac

--- tan-hung-app/src/App.js
+++ tan-hung-app/src/App.js
@@ -1,8 +1,12 @@
 import Home from "./pages/Home";
 import "./App.css";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return <Home />;
+  return <ProductList/>;
 }
 
 export default App;
---
+++ tan-hung-app/src/pages/ProductList.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+const Container = styled.div``;
+const Title = styled.h1`margin: 20px`;
+const FilterContainter = styled.div`display: flex; justify-content: space-between`;
+const Filter = styled.div`margin: 20px`;
+
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`;
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`;
+const Option = styled.option``
+const ProductList = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Title>Váy</Title>
+        <FilterContainter>
+            <Filter>
+                <FilterText>Lọc sản phẩm: </FilterText>
+                <Select>
+                <Option disabled selected>
+                        Màu sắc
+                    </Option>
+                    <Option>Trắng</Option>
+                    <Option>Đen</Option>
+                    <Option>Đỏ</Option>
+                    <Option>Xanh</Option>
+                    <Option>Vàng</Option>
+                    <Option>Lục</Option>
+                </Select>
+                <Select>
+                <Option disabled selected>
+                        Kích thước
+                    </Option>
+                    <Option>M</Option>
+                    <Option>L</Option>
+                    <Option>S</Option>
+                    <Option>XS</Option>
+                    <Option>XL</Option>
+                </Select>
+            </Filter>
+            <Filter>
+                <FilterText>Sắp xếp sản phẩm: </FilterText>
+                <Select>
+                    <Option selected>Sản phẩm mới nhất</Option>
+                    <Option>Giá tăng dần</Option>
+                    <Option>Giá giảm dần</Option>
+                </Select>
+            </Filter>
+        </FilterContainter>
+        <Products/>
+        <Newsletter/>
+        <Footer/>
+    </Container>
+  )
+}
+
+export default ProductList
