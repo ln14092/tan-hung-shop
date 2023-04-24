6b9cffb431525d052deeab2c01c9f64b612ddbb2
Lee Minh Tieens <tienminhleminhss@gmail.com>
2023-04-25 02:42:37 +0700

    hoan thanh responesive

--- tan-hung-app/src/components/Navbar.js
+++ tan-hung-app/src/components/Navbar.js
@@ -2,11 +2,13 @@ import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
+import {mobile} from "../responsive";
 import logo from "../images/logo.png";
 // import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height:"50px"})}
 `;
 
 const Wrapper = styled.div`
@@ -14,6 +16,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding:"10px 0px"})}
   height: 100%;
 `;
 
@@ -26,6 +29,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display:"none"})}
 `;
 
 const SearchContainer = styled.div`
@@ -38,6 +42,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width:"50px"})}
 `;
 
 const Center = styled.div`
@@ -53,18 +58,21 @@ const Logo = styled.div`
   background-repeat: no-repeat;
   background-size: contain;
   font-weight: bold;
+  ${mobile({ fontSize:"24px"})}
 `;
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({flex:2 , justifyContent:"center"})}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize:"12px",marginLeft:"10px"})}
 `;
 
 const Navbar = () => {
