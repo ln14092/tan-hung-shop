6438ae9e207e733be846b84240a92f3d1c847de4
ln14092 <ln14092@gmail.com>
2023-05-12 10:27:46 +0700

    fixbug

--- api/index.js
+++ api/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRouter = require("./routes/userRoutes");
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const cartRouter = require("./routes/cartRoutes");
+const orderRouter = require("./routes/orderRoutes");
+const stripeRouter = require("./routes/stripeRoutes");
 const cors = require("cors");
 
 dotenv.config();
@@ -18,8 +22,12 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/checkout", stripeRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("App server is running!");
