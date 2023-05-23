c37f9bd5ee19fd0e787810f87f565bcb831c6ab1
ln14092 <ln14092@gmail.com>
2023-05-23 16:17:05 +0700

    admin interface

---
+++ admin/src/pages/home/Home.js
@@ -0,0 +1,26 @@
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import "./home.css";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { useEffect, useMemo, useState } from "react";
+
+export default function Home() {
+  const [userStats, setUserStats] = useState([]);
+
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userStats}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+}
