c1c8eabea1e98b2f49d9f7ef161ca87d707a5fd0
Lee Minh Tieens <tienminhleminhss@gmail.com>
2023-05-26 02:17:14 +0700

    firebase

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOsIK4QUxZQtix8JNO4QsqBQ1BWX8TLmo",
+  authDomain: "shop-a1f4e.firebaseapp.com",
+  projectId: "shop-a1f4e",
+  storageBucket: "shop-a1f4e.appspot.com",
+  messagingSenderId: "252380080207",
+  appId: "1:252380080207:web:284ddec6392567c7bfe23a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- admin/src/pages/newProduct/NewProduct.js
+++ admin/src/pages/newProduct/NewProduct.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import "./newProduct.css";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import app from "../../firebase"
+import { addProduct } from "../../redux/apiCalls";
+import { useDispatch } from "../../redux/apiCalls";
+
 
 export default function NewProduct() {
   const [inputs, setInputs] = useState({});
   const [file, setFile] = useState(null);
   const [cat, setCat] = useState([]);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setInputs((prev) => {
@@ -14,6 +20,47 @@ export default function NewProduct() {
   const handleCat = (e) => {
     setCat(e.target.value.split(","));
   };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+          default:
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const product = {...inputs,img:downloadURL,categories:cat};
+          addProduct(product,dispatch)
+        });
+      }
+    );
+  }
 
   return (
     <div className="newProduct">
@@ -65,7 +112,7 @@ export default function NewProduct() {
             <option value="false">No</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button onClick={handleClick} className="addProductButton">Create</button>
       </form>
     </div>
   );
